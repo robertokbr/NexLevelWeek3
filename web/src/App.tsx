cf49ad98a324010c65a826550a78bd98905104d5
robertokbr <robertojuniordev@gmail.com>
2020-10-18 02:44:03 -0300

    Initi frontend web application

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+import Routes from './Routes';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <Routes />
+  </>
+);
+
+export default App;
